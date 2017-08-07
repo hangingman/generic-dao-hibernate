0b70cee1feb4ce0bf408560cb36b5a1afd277257
hiroyuki nagata <idiotpanzer@gmail.com>
2017-08-08 01:31:59 +0900

    Add DDL files

---
+++ src/main/resources/db/migration/V3__Append_sample_departments.sql
@@ -0,0 +1,16 @@
+--
+-- SQLs from https://www.ntu.edu.sg/home/ehchua/programming/sql/SampleDatabases.html
+--
+USE sample;
+
+INSERT INTO `departments` VALUES 
+('d001','Marketing'),
+('d002','Finance'),
+('d003','Human Resources'),
+('d004','Production'),
+('d005','Development'),
+('d006','Quality Management'),
+('d007','Sales'),
+('d008','Research'),
+('d009','Customer Service');
+
